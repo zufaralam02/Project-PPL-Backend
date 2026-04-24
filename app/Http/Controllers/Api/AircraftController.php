16e2afe1d2219958196f563deb93e7ec4d44f0a6
Rofiif Zufar Allam <zufaralam02@gmail.com>
2026-04-24 20:02:17 +0700

    add filter id in get list aircraft

--- app/Http/Controllers/Api/AircraftController.php
+++ app/Http/Controllers/Api/AircraftController.php
@@ -5,15 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Aircraft;
 use App\Helpers\ResponseHelper;
+use Illuminate\Http\Request;
 
 class AircraftController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // $aircraft = Aircraft::select('id', 'name')->get();
-        $aircraft = Aircraft::select('id', 'name')
-            ->orderBy('id', 'asc')
-            ->get();
+        $query = Aircraft::select('id', 'name')
+            ->orderBy('id', 'asc');
+
+        // 🔥 filter by id (optional)
+        if ($request->has('id')) {
+            $query->where('id', $request->id);
+        }
+
+        $aircraft = $query->get();
 
         if ($aircraft->isEmpty()) {
             return ResponseHelper::success([], 'No aircraft found');
